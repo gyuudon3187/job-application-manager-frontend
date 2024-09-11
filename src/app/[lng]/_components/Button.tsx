a47aefb717333d121468e02540226691d65ff413
Daniel Dahlberg <ddahlb@kth.se>
2024-09-11 17:30:18 +0200

    Adjusted the custom Button component so that it accepts children.

--- src/app/[lng]/_components/Button.tsx
+++ src/app/[lng]/_components/Button.tsx
@@ -1,15 +1,17 @@
-import { MouseEventHandler } from "react";
+import { ButtonHTMLAttributes } from "react";
 
 export default function Button({
-  text,
-  onSubmit,
-}: Readonly<{ text: string; onSubmit: MouseEventHandler<HTMLButtonElement> }>) {
+  children,
+  onClick,
+  ...rest
+}: Readonly<ButtonHTMLAttributes<HTMLButtonElement>>) {
   return (
     <button
       class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-      onClick={onSubmit}
+      onClick={onClick}
+      {...rest}
     >
-      {text}
+      {children}
     </button>
   );
 }
