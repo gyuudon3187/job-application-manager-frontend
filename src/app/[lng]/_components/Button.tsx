33aae1d5ae3dad44e5b9581250abc346a926e66b
Daniel Dahlberg <ddahlb@kth.se>
2024-09-13 22:24:25 +0200

    Shortened the transition duration because it looked unnatural when switching to dark mode.

--- src/app/[lng]/_components/Button.tsx
+++ src/app/[lng]/_components/Button.tsx
@@ -35,7 +35,7 @@ export default function Button({
 
   return (
     <button
-      className={`relative transition-all duration-300 ease-in-out text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 
+      className={`relative duration-100 ease-in-out text-gray-900 bg-white border border-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 
         dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 
         focus:outline-none focus:ring-gray-100 hover:bg-gray-100 focus:ring-0
         ${disabled || loading ? "opacity-50 cursor-not-allowed hover:bg-white dark:opacity-50 dark:cursor-not-allowed dark:hover:bg-gray-800" : ""}
